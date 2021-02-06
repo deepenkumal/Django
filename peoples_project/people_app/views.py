c96cdc58f77eece961f1e18e24727f25681acb55
deepenkumal <deepenkumal@noreply.github.com>
2021-02-06 21:34:33 +0545

    data saved from form

--- peoples_project/people_app/views.py
+++ peoples_project/people_app/views.py
@@ -7,6 +7,11 @@ def index(request):
     return render(request,'index.html')
 
 def addpeople(request):
+    if request.method=="POST":
+        form=PeopleForm(request.POST,request.FILES)
+        if form.is_valid():
+            print("Valid")
+
     form=PeopleForm()
     return render(request,'addpeople.html',{'form':form})
 
