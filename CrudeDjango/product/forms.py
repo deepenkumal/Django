efbf982b28deea7b9f28d2a6e608e41c8d45ba95
deepenkumal <47030886+deepenkumal@users.noreply.github.com>
2021-02-06 09:06:25 +0545

    bootstrap widgets added to model forms

--- CrudeDjango/product/forms.py
+++ CrudeDjango/product/forms.py
@@ -10,6 +10,11 @@ class AddProduct(forms.ModelForm):
     class Meta:
         model = Product
         fields =('product_name','product_detail','product_image')
+        widgets={
+            'product_name':forms.TextInput(attrs={'class':'form-control'}),
+            'product_details':forms.TextInput(attrs={'class':'form-control'}),
+            'product_image':forms.FileInput(attrs={'class':'form-control'}),
+        }
 
 class ConfirmForm(forms.Form):
     pass
