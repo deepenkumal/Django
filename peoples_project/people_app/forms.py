4c1efc381fdd3a77c345eabac27d16d25daac45c
deepenkumal <deepenkumal@noreply.github.com>
2021-02-06 21:12:11 +0545

    bootstrap widgets added

--- peoples_project/people_app/forms.py
+++ peoples_project/people_app/forms.py
@@ -6,3 +6,9 @@ class PeopleForm(forms.ModelForm):
     class Meta:
         model=People
         fields=['name','country','age','image']
+        widgets={
+            'name':forms.TextInput(attrs={'class':'from form-control'}),
+            'country':forms.TextInput(attrs={'class':'form-control'}),
+            'age':forms.TextInput(attrs={'class':'form-control'}),
+            'image':forms.FileInput(attrs={'class':'form-control'})
+        }
